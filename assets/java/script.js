eb55d0a081e272c93eef3805f6ed0f15f4b1d930
KevJohBer <kevin.bergstrom0313@gmail.com>
2022-10-03 14:30:44 +0000

    add style to sections and add javascript functions

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,1 +1,17 @@
-console.log('hello')
+document.addEventListener('DOMContentLoaded', generateRandomWord)
+
+let textBox = document.getElementById('paragraph').textContent
+textBox.textContent = 'bruh'
+
+
+/**generates a paragraph of text */
+function generateRandomWord (){
+    let text = ''
+    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+
+    for (letter of possible) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
+    return text;
+}
+
