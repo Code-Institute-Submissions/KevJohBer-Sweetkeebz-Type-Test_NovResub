90526c9dbd1dced4eac22a3277966ea3eaa71df7
KevJohBer <kevin.bergstrom0313@gmail.com>
2022-10-03 15:47:45 +0000

    Add javascript code that generates a paragraph of random words

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,17 +1,14 @@
 document.addEventListener('DOMContentLoaded', generateRandomWord)
 
-let textBox = document.getElementById('paragraph').textContent
-textBox.textContent = 'bruh'
-
-
 /**generates a paragraph of text */
 function generateRandomWord (){
     let text = ''
-    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-
-    for (letter of possible) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length))
+    let words = ['you ', "don't ", 'feel ', 'so ', 'far ', 'away ', 'well ', 'I ', 'was ', 'cruising ', 'down ', 'the ', 'street ', 'we ', 'will ', 'be ', 'alright ', 'man ', 'used ', 'to ', 'stay ', 'awake ', 'all ', 'night ', 'and ', 'wonder ', 'if ', 'worth ', 'fight ']
+    for (i = 0; i < 50; i++) {
+        text += words[Math.floor(Math.random() * 10)]
     }
-    return text;
+    return text
 }
 
+let textBox = document.getElementById('paragraph')
+textBox.textContent = generateRandomWord()
