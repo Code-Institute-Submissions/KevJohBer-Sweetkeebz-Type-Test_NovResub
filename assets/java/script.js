1074b8787012b7c4957b00ed62b1ad60991a9bfe
KevJohBer <kevin.bergstrom0313@gmail.com>
2022-10-04 14:29:04 +0000

    make text and input field responsive so that user sees where he is at

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,14 +1,49 @@
 document.addEventListener('DOMContentLoaded', generateRandomWord)
 
-/**generates a paragraph of text */
+textDisplayElement = document.getElementById('paragraph')
+
+textArea = document.querySelector('.text-area')
+
+inputField = document.querySelector('.text-area .input-field')
+
+let charIndex = 0;
+
+/**
+ * generates a paragraph of text 
+ * */
 function generateRandomWord (){
     let text = ''
     let words = ['you ', "don't ", 'feel ', 'so ', 'far ', 'away ', 'well ', 'I ', 'was ', 'cruising ', 'down ', 'the ', 'street ', 'we ', 'will ', 'be ', 'alright ', 'man ', 'used ', 'to ', 'stay ', 'awake ', 'all ', 'night ', 'and ', 'wonder ', 'if ', 'worth ', 'fight ']
     for (i = 0; i < 50; i++) {
-        text += words[Math.floor(Math.random() * 10)]
-    }
+        text += words[Math.floor(Math.random() * words.length)]
+    }   
     return text
 }
 
-let textBox = document.getElementById('paragraph')
-textBox.textContent = generateRandomWord()
+async function generateNewRandomWord() {
+    let paragraph = generateRandomWord()
+    paragraph.split('').forEach(character => {
+        let textSpan = document.createElement('span')
+        textSpan.innerText = character
+        textDisplayElement.appendChild(textSpan) 
+    })
+    document.addEventListener('keydown', () => inputField.focus())
+    textDisplayElement.addEventListener('click', () => inputField.focus())
+}
+
+function initTyping() {
+    let characters = textDisplayElement.querySelectorAll('span');
+    let typedChar = inputField.value.split('')[charIndex]
+    if(characters[charIndex].innerText === typedChar) {
+        characters[charIndex].classList.add('correct')     
+    } else {
+        characters[charIndex].classList.add('incorrect')
+    }
+    charIndex++;
+
+    characters.forEach(span => span.classList.remove('active'))
+    characters[charIndex].classList.add('active')
+}
+
+generateNewRandomWord()
+inputField.addEventListener('input', initTyping);
